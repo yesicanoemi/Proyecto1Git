4ebbbf9db363a74ce6e8dba36711e453f737c632
yesica <yesicargueta@gmail.com>
2015-06-08 10:46:36 -0600

    Imprimir Hola mundo 2 en Consola

--- src/proyectogit/ProyectoGit.java
+++ src/proyectogit/ProyectoGit.java
@@ -17,6 +17,7 @@ public class ProyectoGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
     
 }
